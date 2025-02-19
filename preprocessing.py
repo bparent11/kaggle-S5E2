851d43c21601bbd17f146064fea670489294e531
bparent11 <benjamin.parent@etu.minesparis.psl.eu>
2025-02-19 21:30:34 +0100

    Addition of columns to inform that the initial value was missing

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,27 @@ import pandas as pd
 import numpy as np
 
 def preprocess(df):
-    
+
     df[df.select_dtypes(include='object').columns] = df.select_dtypes(include='object').fillna('Inconnu')
     df[df.select_dtypes(include='number').columns] = df.select_dtypes(include='number').fillna(-1)
+
+    numerical_columns = df.select_dtypes(include='number')
+    categorical_columns = df.select_dtypes(include='object')
+
+    for column in numerical_columns:
+        if (df[column] == -1).sum() > 0:
+            missing_column_name = f"{column}_Missing"
+            df[missing_column_name] = (df[column] == -1).astype(int)
+
+    for column in categorical_columns:
+        if (df[column] == 'Inconnu').sum() > 0:
+            missing_column_name = f"{column}_Missing"
+            df[missing_column_name] = (df[column] == 'Inconnu').astype(int)
+
+    df.loc[df['Laptop Compartment'] == "Yes", "Laptop Compartment"] = int(1)
+    df.loc[df['Laptop Compartment'] == "No", "Laptop Compartment"] = int(0)
+
+    df.loc[df['Waterproof'] == "Yes", "Waterproof"] = int(1)
+    df.loc[df['Waterproof'] == "No", "Waterproof"] = int(0)
 
     return df

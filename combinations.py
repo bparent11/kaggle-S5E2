6266c9228ae85c613153f8445425831c87e923c5
bparent11 <benjamin.parent@etu.minesparis.psl.eu>
2025-02-22 10:53:07 +0100

    useful functions, will develop my knowledge about defining classes to have a better control on what I'm doing

--- combinations.py
+++ combinations.py
@@ -3,50 +3,42 @@ from sklearn.model_selection import cross_val_score, KFold
 import xgboost as xgb
 import time
 
-# combinations = list(product(X.columns, X.columns)) # cartesian product
-columns_combinations = list(combinations(X.columns, 2)) # combinations without duplicates
-print(len(columns_combinations))
-combinations_filtered = list(filter(lambda tuple: tuple[0].split("_")[0] != tuple[1].split("_")[0], columns_combinations)) #filtered
 
-# Afficher les combinaisons filtrées et leur nombre
-print(combinations_filtered)
-print(len(combinations_filtered))
+def finding_best_combinations_in_pairs(X, y):
+    # combinations = list(product(X.columns, X.columns)) # cartesian product
+    columns_combinations = list(combinations(X.columns, 2)) # combinations without duplicates
+    combinations_filtered = list(filter(lambda tuple: tuple[0].split("_")[0] != tuple[1].split("_")[0], columns_combinations)) #filtered
 
-combination_names = [f"{tpl[0]}xxx{tpl[1]}" for tpl in combinations_filtered]
-print(combination_names)
-print(len(combination_names))
+    combination_names = [f"{tpl[0]}xxx{tpl[1]}" for tpl in combinations_filtered]
 
-test_score_dict = {}
-KF = KFold(n_splits=2, shuffle=True, random_state=42)
+    test_score_dict = {}
+    KF = KFold(n_splits=2, shuffle=True, random_state=42)
 
-for index, new_col in enumerate(combination_names, len(combination_names)-len(combination_names)):
-    start_time = time.time()
-    temporary_X = X.copy()
-    separation = new_col.split("xxx")
-    first_col = separation[0]
-    second_col = separation[1]
-    temporary_X[new_col] = temporary_X[first_col] * temporary_X[second_col]
+    print(f"Computation begins, there is {len(combinations_filtered)} combinations to compute !")
+    for index, new_col in enumerate(combination_names, len(combination_names)-len(combination_names)):
+        start_time = time.time()
+        temporary_X = X.copy()
+        separation = new_col.split("xxx")
+        first_col = separation[0]
+        second_col = separation[1]
+        temporary_X[new_col] = temporary_X[first_col] * temporary_X[second_col]
+        model = xgb.XGBRegressor()
+        cv_results = cross_val_score(model, temporary_X, y, cv=KF, scoring='neg_root_mean_squared_error', n_jobs=-1)
+        print(f"cross validation {index + 1} / {len(combination_names)} done !")
+        test_score_dict[new_col] = cv_results.mean()
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Temps écoulé : {elapsed_time:.6f} secondes")
+
+    new_col = "no_column_added"
     model = xgb.XGBRegressor()
-    cv_results = cross_val_score(model, temporary_X, y, cv=KF, scoring='neg_root_mean_squared_error', n_jobs=-1)
-    print(f"cross validation {index + 1} / {len(combination_names)} done !")
+    cv_results = cross_val_score(model, X, y, cv=KF, scoring='neg_root_mean_squared_error', n_jobs=-1)
     test_score_dict[new_col] = cv_results.mean()
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Temps écoulé : {elapsed_time:.6f} secondes")
+    sorted_dict = dict(sorted(test_score_dict.items(), key=lambda item: item[1], reverse=True))
 
-new_col = "no_column_added"
-model = xgb.XGBRegressor()
-cv_results = cross_val_score(model, X, y, cv=KF, scoring='neg_root_mean_squared_error', n_jobs=-1)
-test_score_dict[new_col] = cv_results.mean()
-
-
-sorted_dict = dict(sorted(test_score_dict.items(), key=lambda item: item[1]))
-
-import json
-file_path = 'test_scores_sorted_cv=4_300k_lines.json'
-with open(file_path, 'w') as json_file:
-    json.dump(sorted_dict, json_file, indent=4)
-
-
-## 44 minutes to do it
+    import json
+    file_path = 'best_combinations_22_02.json'
+    with open(file_path, 'w') as json_file:
+        json.dump(sorted_dict, json_file, indent=4)
--- preprocessing.py
+++ preprocessing.py
@@ -3,7 +3,8 @@ import numpy as np
 import json
 
 def preprocess(df, submission=False):
-    global target_encoding_dict
+    global target_encoding_dict, train_columns
+
     df.loc[df['Laptop Compartment'] == "Yes", "Laptop Compartment"] = int(1)
     df.loc[df['Laptop Compartment'] == "No", "Laptop Compartment"] = int(0)
     df['Laptop Compartment'] = df['Laptop Compartment'].astype(float)
@@ -31,25 +32,36 @@ def preprocess(df, submission=False):
     # because that's already done with line 6 and OHE.
 
     """TARGET ENCODING | Mean, Count, STD and VAR"""
+    "nunique", "median", "min", "max", "skew"
 
     if submission == False:
         target_encoding_dict = {}
         for column in categorical_columns:
             gb = df.groupby(column).agg(
-                Price_mean = ("Price", "mean"),
-                Count = ("Price", "count"),
-                STD = ("Price", "std"),
-                VAR = ("Price", "var")
+                TEmean = ("Price", "mean"),
+                TECount = ("Price", "count"),
+                TESTD = ("Price", "std"),
+                TEVAR = ("Price", "var"),
+                TEnunique = ("Price", "nunique"),
+                TEmedian = ("Price", "median"),
+                TEmin = ("Price", "min"),
+                TEmax = ("Price", "max"),
+                TEskew = ("Price", "skew")
             )
 
             for index in gb.index:
                 key = f"{column}_{index}"
-                mean = gb.loc[index, "Price_mean"]
-                count = gb.loc[index, "Count"]
-                stdeviation = gb.loc[index, "STD"]
-                var = gb.loc[index, "VAR"]
+                mean = gb.loc[index, "TEmean"]
+                count = gb.loc[index, "TECount"]
+                stdeviation = gb.loc[index, "TESTD"]
+                var = gb.loc[index, "TEVAR"]
+                nunique = gb.loc[index, "TEnunique"]
+                median = gb.loc[index, "TEmedian"]
+                min = gb.loc[index, "TEmin"]
+                max = gb.loc[index, "TEmax"]
+                skew = gb.loc[index, "TEskew"]
 
-                target_encoding_dict[key] = [mean, count, stdeviation, var]
+                target_encoding_dict[key] = [mean, count, stdeviation, var, nunique, median, min, max, skew]
                 #print(key)
                 #print(target_encoding_dict[key])
             
@@ -59,6 +71,8 @@ def preprocess(df, submission=False):
             # Joindre les résultats au DataFrame principal
             df = df.merge(gb, on=column, how='left')
 
+        train_columns = df.columns
+        print(train_columns)
 
     if submission == True:
         for key in target_encoding_dict:
@@ -66,8 +80,8 @@ def preprocess(df, submission=False):
             corresponding_value = key.split("_")[1]
             
             iteration=0
-            for aggfunc in ["Price_mean", "Count", "STD", "VAR"]:
-                df.loc[df[initial_column] == corresponding_value, f"{initial_column}_{aggfunc}"] = target_encoding_dict[key][iteration]
+            for aggfunc in ["mean", "Count", "STD", "VAR", "nunique", "median", "min", "max", "skew"]:
+                df.loc[df[initial_column] == corresponding_value, f"{initial_column}_TE{aggfunc}"] = target_encoding_dict[key][iteration]
                 iteration += 1
 
     #with open('test_scores_sorted_cv=2_150k_lines.json', 'r') as json_file:
